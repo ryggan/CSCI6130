965cf916dd206c7a5aae05adb06d3289c8bdb0f8
Andreas Carlsson <andreasc89@gmail.com>
2019-01-30 22:32:18 +0800

    Print the input, actual result and expected result

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import brainfuck
 import compiler
+import reward
 from utils import prepare_list
 
 # Input list
 unsorted_list = [9, 1, 6, 3]
 
 code = prepare_list(unsorted_list)
-
 code = compiler.read(code)
 
 output = str(compiler.eval(code))
 output = list(map(lambda x: int(x), output))
 
+print("Reward: " + reward.evaluate_sorting(output))
 print("Unsorted: " + str(unsorted_list))
 print("Output: " + str(output))
 unsorted_list.sort()
