f06cc3d733d5308821ace55253e616671dc572df
Andreas Carlsson <andreasc89@gmail.com>
2019-01-30 22:34:59 +0800

    Add a (very) simple reward function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import reward
 from utils import prepare_list
 
 # Input list
-unsorted_list = [9, 1, 6, 3]
+unsorted_list = [9, 1, 6, 3, 2]
 
 code = prepare_list(unsorted_list)
 code = compiler.read(code)
@@ -12,7 +12,7 @@ code = compiler.read(code)
 output = str(compiler.eval(code))
 output = list(map(lambda x: int(x), output))
 
-print("Reward: " + reward.evaluate_sorting(output))
+print("Reward: " + str(reward.evaluate_sorting(output)))
 print("Unsorted: " + str(unsorted_list))
 print("Output: " + str(output))
 unsorted_list.sort()
